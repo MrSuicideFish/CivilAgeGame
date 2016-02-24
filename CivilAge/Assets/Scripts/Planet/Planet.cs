b4de788b008fef4e966cdb85437bdebb9f4674cd
granthum123 <granthum@live.unc.edu>
2016-02-24 15:43:52 -0500

    no message

---
+++ CivilAge/Assets/Scripts/Planet/Planet.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet : MonoBehaviour
+{
+    int galaxyScale = 100;
+    int PlanetScaleFactor = 100;
+    void start()
+    {
+        //Create randomly sized planet, with set of tiles covering at most half the planet
+        int PlanetSize = Random.Range(2, 20) * PlanetScaleFactor;
+        Vector3 Position = new Vector3(Random.Range(0, galaxyScale * 10000), Random.Range(0, galaxyScale * 10000), Random.Range(0, galaxyScale * 10000));
+    }
+
+}
