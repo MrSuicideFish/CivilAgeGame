17cc899d3ff8cb420eac3b2ab7e18f02274591c0
Malcolm Neal <Malcolm259@Gmail.com>
2016-02-24 15:26:05 -0500

    added camera controller stuff

--- CivilAge/Assets/Scripts/CameraController.cs
+++ CivilAge/Assets/Scripts/CameraController.cs
@@ -17,8 +17,12 @@ public class CameraController : MonoBehaviour
 
     public float CameraMoveSpeed { get; private set; }
     public float CameraRotationSpeed { get; private set; }
+    public float CameraZoomSpeed { get; private set; }
 
+    public float MinZoomDistance { get; private set; }
     public float ZoomDistance { get; private set; }
+    public float MaxZoomDistance { get; private set; }
+
     public float RotationAngle { get; private set; }
 
     void Awake( )
@@ -30,11 +34,18 @@ public class CameraController : MonoBehaviour
         CameraMoveSpeed = 10;//PlayerPrefs.GetFloat( "CameraMovementSpeed", 10 );
         CameraRotationSpeed = 10;//PlayerPrefs.GetFloat( "CameraMovementSpeed", 10 );
 
+        //Zoom
+        MinZoomDistance = 0;
+        MaxZoomDistance = 30;
+        CameraZoomSpeed = 10;
+
         //Debug
         ZoomDistance = 5;
-        RotationAngle = 90;
+        RotationAngle = 0;
     }
 
+    private Vector3 DragStartPosition = Vector3.zero;
+    private Vector3 DragPosition = Vector3.zero;
     void Update( )
     {
         if ( !LockEnabled )
@@ -44,8 +55,38 @@ public class CameraController : MonoBehaviour
             _moveDirection.x    = Input.GetAxis( "Horizontal" ) * ( CameraMoveSpeed * Time.deltaTime );
             _moveDirection.z    = Input.GetAxis( "Vertical" ) * ( CameraMoveSpeed * Time.deltaTime );
 
+            //Mouse wheel scroll
+            var _scrollValue = -Input.GetAxis( "Mouse ScrollWheel" ) * CameraZoomSpeed;
+
+            if ( _scrollValue != 0 )
+            {
+                //zoom camera
+                var _newZoom = ZoomDistance + _scrollValue;
+                if( _newZoom > MinZoomDistance && _newZoom < MaxZoomDistance )
+                {
+                    ZoomDistance = _newZoom;
+                }
+            }
+
+            //Mouse dragging stuff - Selection
+
+
+            //Mouse dragging stuff - Rotation
+            if ( Input.GetMouseButtonDown( 2 ) )
+            {
+                DragStartPosition = Input.mousePosition;
+            }
+
+            if ( Input.GetMouseButton( 2 ) )
+            {
+                DragPosition = Input.mousePosition;
+
+                //process drag
+                RotationAngle += ( DragPosition.x - DragStartPosition.x ) * 0.0006f;
+            }
+
             //Transform move direction and increment location
-            _moveDirection = transform.InverseTransformVector( _moveDirection );
+            _moveDirection = transform.TransformDirection( _moveDirection );
             TargetLocation.x += _moveDirection.x;
             TargetLocation.z += _moveDirection.z;
         }
@@ -63,8 +104,8 @@ public class CameraController : MonoBehaviour
             _viewtargetPos.z + ZoomDistance * Mathf.Sin( RotationAngle + Mathf.PI / 180 )
         );
 
-        transform.position = Vector3.Lerp( transform.position, newPos, CameraMoveSpeed * Time.deltaTime );
-        transform.rotation = Quaternion.Lerp( transform.rotation, Quaternion.LookRotation( _viewtargetPos - transform.position ), 0.2f );
+        transform.position = Vector3.Lerp( transform.position, newPos, 0.5f );
+        transform.rotation = Quaternion.LookRotation( _viewtargetPos - transform.position );
     }
 
     void FixedUpdate( )
