6302e327455c5f2af2b3c33e2306c5c016990b1b
Malcolm Neal <Malcolm259@Gmail.com>
2016-02-24 12:56:21 -0500

    added ship and actor scripts

---
+++ CivilAge/Assets/Scripts/System/CivInterfaces.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public interface ISelectable
+{
+    void Select( );
+    void Deselect( );
+
+    event EventHandler<EventArgs> ActorSelected;
+    event EventHandler<EventArgs> ActorDeselected;
+
+    void OnActorSelected( );
+    void OnActorDeselected( );
+}
