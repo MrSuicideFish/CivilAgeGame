0f0e73cc6485f44ab255262bc8e9eb2f3f17d2f7
Malcolm Neal <Malcolm259@Gmail.com>
2016-02-25 16:06:48 -0500

    adding rect selection logic (incomplete)

--- CivilAge/Assets/Scripts/System/PlayerInputController.cs
+++ CivilAge/Assets/Scripts/System/PlayerInputController.cs
@@ -45,13 +45,79 @@ public class PlayerInputController : MonoBehaviour
         switch ( DragMode )
         {
             case MouseDragMode.DRAG_SELECT:
+
+                var pos = new Vector2( DragStartPosition.x, DragStartPosition.y );
+                var size = DragPosition - DragStartPosition;
+
+                //Process rect
+                Rect selectionRect = new Rect( pos, size );
+
+                //trace to world
+                float dist = 10f;
+
+                //Top Left Pos
+                var rectTopLeft = new Vector3( selectionRect.x, Screen.height - selectionRect.y, dist );
+                rectTopLeft = Camera.main.ScreenToWorldPoint( rectTopLeft );
+
+                //bottom left pos
+                var rectBotLeft = new Vector3( selectionRect.x, Screen.height - ( selectionRect.y + selectionRect.height ), dist );
+                rectBotLeft = Camera.main.ScreenToWorldPoint( rectBotLeft );
+
+                //bottom right pos
+                var rectBotRight = new Vector3( selectionRect.x + selectionRect.width, Screen.height - ( selectionRect.y + selectionRect.height ), dist );
+                rectBotRight = Camera.main.ScreenToWorldPoint( rectBotRight );
+
+                //top right
+                var rectTopRight = new Vector3( selectionRect.x + selectionRect.width, Screen.height - selectionRect.y, dist );
+                rectTopRight = Camera.main.ScreenToWorldPoint( rectTopRight );
+
+                //Center
+                var rectCenter = new Vector3( selectionRect.x + (selectionRect.width / 2), Screen.height - ( selectionRect.y + selectionRect.height / 2 ), dist );
+                rectCenter = Camera.main.ScreenToWorldPoint( rectCenter );
+
+                var width = Vector3.Distance( rectTopLeft, rectTopRight ) * 6;
+                var height = Vector3.Distance( rectTopLeft, rectBotLeft ) * 6;
+
+                RaycastHit[] hitObj = Physics.BoxCastAll( rectCenter, new Vector3( width * 0.5f, height * 0.5f, Camera.main.nearClipPlane ), rectCenter - Camera.main.transform.position, Camera.main.transform.rotation );
+
+                foreach(RaycastHit hit in hitObj )
+                {
+                    hit.collider.gameObject.GetComponent<MeshRenderer>( ).material.color = Color.red;
+                }
+
+                if ( !DebugSphereA )
+                {
+                    float scale = 0.2f;
+
+                    DebugSphereA = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                    DebugSphereA.transform.localScale = Vector3.one * scale;
+
+                    DebugSphereB = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                    DebugSphereB.transform.localScale = Vector3.one * scale;
+
+                    DebugSphereC = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                    DebugSphereC.transform.localScale = Vector3.one * scale;
+
+                    DebugSphereD = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                    DebugSphereD.transform.localScale = Vector3.one * scale;
+
+                    DebugSphereE = GameObject.CreatePrimitive( PrimitiveType.Cube );
+                    DebugSphereE.transform.localScale = Vector3.one * scale;
+                }
+
+                DebugSphereA.transform.position = rectTopLeft;
+                DebugSphereB.transform.position = rectBotLeft;
+                DebugSphereC.transform.position = rectBotRight;
+                DebugSphereD.transform.position = rectTopRight;
+                DebugSphereE.transform.position = rectCenter;
+
                 break;
 
             case MouseDragMode.DRAG_CAMERA_ROTATE:
 
                 var diff = ( DragPosition.x - DragStartPosition.x ) / Screen.width;
                 CameraController.CurrentCamera.RotateCamera( diff );
-                break;
+            break;
 
             default:
                 break;
@@ -94,6 +160,12 @@ public class PlayerInputController : MonoBehaviour
     }
 
     Texture2D BoxTex;
+    GameObject DebugSphereA,
+                DebugSphereB,
+                DebugSphereC,
+                DebugSphereD,
+                DebugSphereE;
+
     void OnGUI( )
     {
         //Calculate selecton rect
@@ -110,9 +182,7 @@ public class PlayerInputController : MonoBehaviour
             GUI.color = new Color( 1, 1, 1, 0.05f );
 
             if ( !BoxTex )
-            {
                 BoxTex = Texture2D.whiteTexture;
-            }
 
             GUI.DrawTexture( selectionRect, BoxTex, ScaleMode.StretchToFill );
         }
